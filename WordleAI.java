873e84cdf34fc9a81c5732fb6799038eab08066c
Blake Fordham <104882441+bcfordham@users.noreply.github.com>
2022-10-10 10:57:38 +0800

    Add files via upload

---
+++ WordleAI.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+/**
+ * This class contains static methods that play Wordle using a simple artificial intelligence.
+ *
+ * @author StudentNumber1 AND StudentNumber2
+ */
+public class WordleAI
+{
+    private WordleAI()
+    {
+        // Constructor is private because all methods are static
+        // and instances of WordleAI should not be created
+    }
+    
+    
+    /**
+     * Returns true if guess contains the letter c and false otherwise.
+     */
+    public static boolean guessContains(String guess, char c)
+    {
+        // TODO 1
+        return true;
+    }
+    
+    /**
+     * Returns true if newGuess is consistent with a previousGuess and its result
+     * and false otherwise.
+     * 
+     * The parameter previousGuess is a previous Wordle guess made by the AI.
+     * The parameter result is the result of the guessWord method in WordleGame.
+     * The parameter newGuess is a potential new word to guess.
+     * 
+     * A newGuess is consistent with the previousGuess and result if they can be
+     * explained by newGuess being the secret word. That is, newGuess should not
+     * contradict results from previous guesses.
+     * 
+     * For example, suppose we have previousGuess="dxaxx" and the result="*_.__",
+     * then newGuess="dairy" or newGuess="dzzza" would return true, but
+     * newGuess="testa" or newGuess="dxiry" would be false.
+     * This is because only newGuess="dairy" or newGuess="dzzza" could have been the secret word
+     * for previousGuess="dxaxx" to get result="*_.__"
+     * 
+     * HINT: Can you use a new WordleGame(...) somehow?
+     */
+    public static boolean isConsistent(String previousGuess, String result, String newGuess)
+    {
+        // TODO 2
+        return true;
+    }
+    
+    /**
+     * Returns true if result contains only '*' characters
+     * and false if it contains a non-'*' character.
+     */
+    public static boolean isAllStars(String result)
+    {
+        // TODO 3
+        return true;
+    }
+    
+    /**
+     * This method runs the AI algorithm.
+     * Given a dictionary and a game, makes a series of calls to game.guessWord(word)
+     * to find the secret word in game.
+     * Returns an ArrayList containing the words in the order they were guessed.
+     * If the secret word could not be found, returns null.
+     * 
+     * The AI algorithm is very specific!
+     * It uses a simple strategy similar to one you may have used when playing Wordle.
+     * 
+     * The AI starts by guessing the the lexicographicaly smallest word.
+     * 
+     * Then, for every guess after that, the AI guesses a word that does not contradict any previous
+     * results it has seen. That is, it makes guesses that are consistent (see isConsistent)
+     * with all guesses made so far.
+     * 
+     * If there are multiple possible guesses that are consistent, then the AI
+     * will pick the lexicographically smallest option.
+     * 
+     * WordleGame has been modified to only allow 6 guesses. After this, it will return an
+     * empty string "".
+     * 
+     * If the game ends because the AI has run out of guesses, then findWord returns null.
+     * Otherwise, findWord returns once it has made a correct guess. This can
+     * be checked using the isAllStars method above. In this case, a list of the AI's
+     * guesses in the order they were made is returned.
+     * 
+     * HINT 1: You will almost certainly need to read the unit test and use the debugger for this method.
+     * HINT 2: See Collections.sort in the Java class libraries for lexicographical ordering.
+     */
+    public static ArrayList<String> findWord(WordleDictionary dictionary, WordleGame game)
+    {
+        // TODO 4
+        return null;
+    }
+}
---
+++ WordleAIAnalyser.java
@@ -0,0 +1,182 @@
+import java.util.*;
+
+/**
+ * Analyses a WordleAI by running experiments and computing statistics.
+ * 
+ * @author StudentNumber1 AND StudentNumber2
+ */
+public class WordleAIAnalyser
+{
+    
+    // Do not modify the fields of this class.
+    private WordleDictionary dictionary;
+    private int wordLength;
+    // You should read but NOT modify the WordleExperimentResult class
+    private ArrayList<WordleExperimentResult> experimentResults;
+    
+    /**
+     * Constructor takes a dictionary and word length to run experiments with.
+     */
+    public WordleAIAnalyser(WordleDictionary dictionary, int wordLength)
+    {
+        // TODO 5
+    }
+    
+    /**
+     * !!! DO NOT MODIFY !!!
+     * This method has been implemented for you.
+     */
+    public ArrayList<WordleExperimentResult> getExperimentResults()
+    {
+        return experimentResults;
+    }
+    
+    /**
+     * Runs an experiment on a word and stores the result in experimentResults.
+     * An experiment is the WordleExperimentResult from using the WordleAI on a WordleGame
+     * with word as the secret word.
+     * 
+     * No checks or guards are needed on the word parameter.
+     * It is always assumed to be the right length and to come from the dictionary.
+     */
+    public void runExperiment(String word)
+    {
+        // TODO 6
+    }
+    
+    /**
+     * Runs and stores experiments for each word in the dictionary with the right length.
+     * 
+     * Should call runExperiment once for each word.
+     */
+    public void runExperimentsWithAllWords()
+    {
+        // TODO 7
+    }
+    
+    /**
+     * Runs and stores experiments for each word in the dictionary with the right length.
+     * 
+     * Only uses words that are lexicographically between the start and finish.
+     * A word is only used if it is the same as start or comes after AND it is the same as finish or comes before.
+     * 
+     * For example, if our words are "act", "bat", "bet", "cat"
+     * Then runExperimentsWithWordsBetween("baa", "caa")
+     * would only run experiments for "bat" and "bet"
+     * 
+     * Should call runExperiment once for each word.
+     * 
+     * HINT: Recall the String compareTo method.
+     */
+    public void runExperimentsWithWordsBetween(String start, String finish)
+    {
+        // TODO 8
+    }
+    
+    /**
+     * Returns a list of all experiment words that were not solved by WordleAI.
+     * The returned list of words should be in lexicographic order.
+     * 
+     * There may be duplicates in experimentResults.
+     * This method should NOT return any duplicated words!
+     * 
+     * HINT 1: Remeber that findWord returns null when it cannot solve the word.
+     * HINT 2: See Collections.sort and the ArrayList contains method.
+     */
+    public ArrayList<String> getUnsolvedWords()
+    {
+        // TODO 9
+        return null;
+    }
+    
+    /**
+     * Returns an array with length 26.
+     * The entry at index [0] is the number of 'a' characters guessed over all experiments by WordleAI.
+     * The entry at index [1] is the number of 'b' characters.
+     * ...
+     * The entry at index [25] is the number of 'z' characters.
+     * 
+     * For example, if the WordleAI guessed the words "cat"+"hat" in one experiment and "log"+"cat" in another:
+     * The return array would be {3, 0, 2, ... }
+     * Which means 3 'a' characters, 0 'b' characters, 2 'c' characters, and so on...
+     * 
+     * HINT: Unsolved words have no guesses and should be skipped.
+     */
+    public int[] getGuessLetterFrequency()
+    {
+        // TODO 10
+        return null;
+    }
+    
+    /**
+     * Returns an array of length 7.
+     * 
+     * The entry at index [0] is the number of times the WordleAI guessed a word correctly after 1 guess.
+     * The entry at index [1] is the number of times exactly 2 guesses were needed.
+     * and so on.
+     * The entry at index [6] is the number of times the WordleAI did not correctly guess the word.
+     */
+    public int[] getNumGuessesFrequency()
+    {
+        // TODO 11
+        return null;
+    }
+    
+    /**
+     * Makes a string containing a histogram picture of getNumGuessesFrequency().
+     * 
+     * A possible histogram might look like this:
+     *     ..*....
+     *     ..*..*.
+     *     .**.**.
+     *     .*****.
+     *     ******.
+     *
+     * The stars form bars in a histogram, and the dots represent empty space.
+     * This would correspond to a frequency table of {1, 3, 5, 2, 3, 4, 0}
+     * Recall that the newline '\n' character can be used to encode a line break in a string.
+     * Note that return string should end with a newline '\n' character.
+     * 
+     * Because the numbers can be large, we use a bucketSize.
+     * The height of a bar in the chart is 0 if the corresponding number is in the inclusive range from 0 to bucketSize-1
+     * The height is 1 if the number is in the inclusive range from bucketSize to bucketSize*2-1
+     * The height is 2 if the number is in the inclusive range from bucketSize*2 to bucketSize*3-1
+     * ...and so on.
+     * 
+     * The height of the histogram should be the same as the height of the tallest bar.
+     * 
+     */
+    public String makeHistogram(int bucketSize)
+    {
+        // TODO 12
+        return null;
+    }
+    
+    /**
+     * Prints the string made by makeHistogram(bucketSize)
+     * 
+     * The following code:
+     * WordleAIAnalyser analyser = new WordleAIAnalyser(new WordleDictionary(), 5);
+     * analyser.runExperimentsWithAllWords();
+     * analyser.printHistogram(50);
+     * 
+     * 
+     * Should print this to the terminal:
+     *   ...*...
+     *   ...*...
+     *   ...*...
+     *   ..**...
+     *   ..**...
+     *   ..***..
+     *   ..***..
+     *   ..***..
+     *   ..***..
+     *   .*****.
+     */
+    public void printHistogram(int bucketSize)
+    {
+        // No need to modify this method!
+        // It has been provided for you.
+        System.out.println(makeHistogram(bucketSize));
+    }
+}
